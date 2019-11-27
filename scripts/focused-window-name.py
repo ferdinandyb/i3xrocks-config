09b5baa8812fcf15e525981b241c07ddcb23191e
Bence Ferdinandy <bence@ferdinandy.com>
2019-11-27 10:55:17 +0100

    adding a filter

--- scripts/focused-window-name.py
+++ scripts/focused-window-name.py
@@ -6,6 +6,8 @@ from i3ipc import Connection, Event
 i3 = Connection()
 focused = i3.get_tree().find_focused()
 
-print(focused.name)
-print(focused.name[:10])
+# remove the if if you want everything printed
+if focused.window_instance in ["libreoffice","evince"]:
+    print(focused.name,focused.window_class,focused.window_instance)
+    print(focused.name[:10])
 
